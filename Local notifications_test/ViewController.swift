e50af67fff757fb9f5e258bdf9a5449d12996bad
Testing_author <testing_beta@mail.com>
2020-04-04 20:02:27 +0300

    Implementing the "didReceive" method, which would be called when the user would open the app as a result of tapping a notification

--- Local notifications_test/ViewController.swift
+++ Local notifications_test/ViewController.swift
@@ -11,10 +11,10 @@ import UserNotifications
 import UIKit
 
 class ViewController: UIViewController, UNUserNotificationCenterDelegate {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
@@ -39,8 +39,8 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         center.removeAllPendingNotificationRequests()
         
         let content = UNMutableNotificationContent()
-        content.title = "Wake up call"
-        content.body = "Waaaakeee uuuuuppp already"
+        content.title = "Trezireaaaaa"
+        content.body = "Ai intarziat la job, trezireaaa"
         content.categoryIdentifier = "alarm"
         content.userInfo = ["customData": "customResult"]
         content.sound = .default
@@ -48,11 +48,11 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         var dateComponents = DateComponents()
         dateComponents.hour = 10
         dateComponents.minute = 30
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         
         //Testing of the notification
-//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
- 
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
     }
@@ -61,9 +61,36 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         let center = UNUserNotificationCenter.current()
         center.delegate = self
         
-        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let show = UNNotificationAction(identifier: "show", title: "Arata-mi mai multe...", options: .foreground)
         let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
         
         center.setNotificationCategories([category])
     }
-}
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
+            
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                // this means the user swiped to unlock
+                
+                print ("Default identifier")
+                
+            case "show":
+                print("Arata-mi mai multe...")
+                
+            default:
+                break
+            }
+        }
+        
+        
+        completionHandler()
+    }
+ 
+    }
+
