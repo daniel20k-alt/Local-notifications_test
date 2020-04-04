89c1aa6d5709161a58bd93a38ff7da05df33d97c
Testing_author <testing_beta@mail.com>
2020-04-04 14:50:23 +0300

    Initial Commit

---
+++ Local notifications_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Local notifications_test
+//
+//  Created by DDDD on 04/04/2020.
+//  Copyright © 2020 MeerkatWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
