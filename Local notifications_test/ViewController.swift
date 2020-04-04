388b5e1df0b5af4a24ad24dd79c6a9b7bd2bd2d8
Testing_author <testing_beta@mail.com>
2020-04-04 17:09:23 +0300

    Making the buttons and the methods that such buttons would call for the project purposes.
    Asking user permission to send notificaitons.

--- Local notifications_test/ViewController.swift
+++ Local notifications_test/ViewController.swift
@@ -6,15 +6,54 @@
 //  Copyright © 2020 MeerkatWorks. All rights reserved.
 //
 
+
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
+    
+    @objc func registerLocal() {
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Uraaaa!")
+            } else {
+                print("Okaaay...")
+            }
+        }
+    }
+    
+    @objc func scheduleLocal() {
+        
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Wake up call"
+        content.body = "Waaaakeee uuuuuppp already"
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "customResult"]
+        content.sound = .default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        //Testing of the notification
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+ 
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
 
-
+    }
 }
-
