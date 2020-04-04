2b6e0e1ee4e7b6453b5fa54bc2e8c6375844d425
Testing_author <testing_beta@mail.com>
2020-04-04 19:23:13 +0300

    Allowing users to selected different buttons when accessing the notification, through the classes UNNotificationAction and UNNotificationCategory

--- Local notifications_test/ViewController.swift
+++ Local notifications_test/ViewController.swift
@@ -10,7 +10,7 @@
 import UserNotifications
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +33,7 @@ class ViewController: UIViewController {
     }
     
     @objc func scheduleLocal() {
+        registerCategories()
         
         let center = UNUserNotificationCenter.current()
         center.removeAllPendingNotificationRequests()
@@ -54,6 +55,15 @@ class ViewController: UIViewController {
  
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
-
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
     }
 }
